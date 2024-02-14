5c5aa4117e08a55e8321b9b1d57c648d57c82fa5
Oussama Berredjem <134536117+OussamaBerredjem@users.noreply.github.com>
2024-02-14 22:53:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const app = express();
 const port = 8080;
 
 // Define your GitHub credentials
-const GITHUB_TOKEN = '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';
+const GITHUB_TOKEN = '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';
 const REPO_OWNER = 'OussamaBerredjem';
-const REPO_NAME = 'Thumbnail';
+const REPO_NAME = 'api_upload_java';
 
 const router = express.Router();
 
@@ -20,6 +20,36 @@ app.use(express.static('public'));
 
 app.use(`/`, router);
 
+async function commit(){
+    try {
+        const { default: fetch } = await import('node-fetch'); // Use dynamic import()
+        // Step 1: Create a new file on the local file system
+        fs.writeFileSync(filePath, fileContent);
+
+        // Step 2: Commit the file to GitHub using GitHub API
+        const response = await fetch(`https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/contents/${filePath}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `token ${GITHUB_TOKEN}`
+            },
+            body: JSON.stringify({
+                message: 'Add example.txt via API',
+                content: Buffer.from(fileContent).toString('base64')
+            })
+        });
+
+        // Step 3: Check if the file was committed successfully
+        if (response.status === 201) {
+            console.log('File committed successfully!');
+        } else {
+            const responseData = await response.json();
+            console.error(responseData);
+        }
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
 
 router.get('/commit', async (req, res) => {
     try {
@@ -62,8 +92,16 @@ router.get("/", (req, res) => {
     });
   });
 
+function update() {
+    const intervalInHours = 20;
+    const intervalInMilliseconds = intervalInHours * 60 * 60 * 1000;
+    
+    setInterval(async ()=>{
+        await commit();
+    },intervalInMilliseconds)
+}
 
-
+update()
 
 app.listen(port||process.env.PORT,()=>{
   console.log(port)
