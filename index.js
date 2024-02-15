f205f74bb63176ee16414f8b8da10267119664e6
Oussama Berredjem <134536117+OussamaBerredjem@users.noreply.github.com>
2024-02-15 20:20:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-/* eslint-disable no-undef */
 const express = require('express');
-const fs = require('fs');
 const app = express();
 
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 // Define your GitHub credentials
 const GITHUB_TOKEN = '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';
@@ -12,24 +10,16 @@ const REPO_NAME = 'api_upload_java';
 
 const router = express.Router();
 
-// Define the file content and path
-const filePath = 'example.txt';
-const fileContent = 'This is an example text file content.';
-
 app.use(express.static('public'));
 
 app.use(`/`, router);
 
-
-
 app.get('/commit', async (req, res) => {
     try {
         const { default: fetch } = await import('node-fetch'); // Use dynamic import()
-        // Step 1: Create a new file on the local file system
-        fs.writeFileSync(filePath, fileContent);
 
-        // Step 2: Commit the file to GitHub using GitHub API
-        const response = await fetch(`https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/contents/${filePath}`, {
+        // Step 1: Commit the file to GitHub using GitHub API
+        const response = await fetch(`https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/contents/example.txt`, {
             method: 'PUT',
             headers: {
                 'Content-Type': 'application/json',
@@ -37,11 +27,11 @@ app.get('/commit', async (req, res) => {
             },
             body: JSON.stringify({
                 message: 'Add example.txt via API',
-                content: Buffer.from(fileContent).toString('base64')
+                content: Buffer.from('This is an example text file content.').toString('base64')
             })
         });
 
-        // Step 3: Check if the file was committed successfully
+        // Step 2: Check if the file was committed successfully
         if (response.status === 201) {
             console.log('File committed successfully!');
             res.send('File committed successfully!');
@@ -56,20 +46,12 @@ app.get('/commit', async (req, res) => {
     }
 });
 
-
 router.get("/", (req, res) => {
     res.json({
-      hello: "hi!"
+        hello: "hi!"
     });
-  });
+});
 
-app.get("/hy", (req, res) => {
-    res.json({
-      hello: "hi!"
-    });
-  });
-
-
-app.listen(port||process.env.PORT,()=>{
-  console.log(port)
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
 });
